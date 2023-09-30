5a59af98c0a29e1884df2b586a60a30f50031d6d
S <s>
2023-09-30 18:34:02 +0400

    {"types":"C","runs":3}

--- NinoMagaria.java
+++ NinoMagaria.java
@@ -1,16 +1,16 @@
 import stanford.karel.Karel;
 
-public class NinoMagaria extends Karel{
-	public void run(){
-		while(frontIsClear()){
-			while(noBeepersPresent()){
+public class NinoMagaria extends Karel {
+	public void run() {
+		while (frontIsClear()) {
+			while (noBeepersPresent()) {
 				putBeeper();
 			}
 			move();
 		}
-		
-		
+		putBeeper();
+		turnLeft();
+
 	}
-	
 
 }
