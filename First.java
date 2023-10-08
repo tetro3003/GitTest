97cd46ef6cc678d2d8fbd8f5bb6d1f7e829d13eb
S <s>
2023-10-08 19:41:23 +0400

    {"types":"A","runs":1}

---
+++ First.java
@@ -0,0 +1,20 @@
+package worlds;
+
+import stanford.karel.SuperKarel;
+
+public class First extends SuperKarel {
+	
+	public void run() {
+		
+		move();
+		move();
+		pickBeeper();
+		move();
+		putBeeper();
+	 
+		
+	}
+
+
+
+}
