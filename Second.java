d5429168953913109168fdf493156d7ff2b02acf
S <s>
2023-10-08 19:46:01 +0400

    {"types":"C","runs":1}

--- Second.java
+++ Second.java
@@ -1,4 +1,45 @@
+import stanford.karel.SuperKarel;
 
-public class Second {
+public class Second extends SuperKarel{
+	
+	public void run() {
+		
+		moveToBeepers();
+		pickTenBeepers();
+		move();
+		putTenBeepers();
+		
+		
+	}
+	
+	private void moveToBeepers(){
+		
+		for(int i=0; i<4; i++){
+			
+			move();
+			
+		}
+		
+	}
+	
+	private void pickTenBeepers(){
+		
+		for(int i=0; i<10; i++){
+			
+			pickBeeper();
+			
+		}
+		
+	}
+	
+	private void putTenBeepers(){
+		
+		for(int i=0; i<10; i++){
+			
+			pickBeeper();
+			
+		}
+		
+	}
 
 }
