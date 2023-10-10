7b35f085cc7550a37de82c9cf055d0983cd1544f
S <s>
2023-10-10 16:04:30 +0400

    {"types":"AR","runs":2}

---
+++ CollectNewspaperKarel.java
@@ -0,0 +1,46 @@
+
+
+import stanford.karel.Karel;
+
+public class CollectNewspaperKarel extends Karel {
+	
+	public void run(){
+		
+		wayToBeeper();
+		pickBeeper();
+		backHome();
+		
+	}
+
+	
+	private void wayToBeeper(){
+		
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		
+	}
+	private void backHome(){
+		
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		
+		
+	}
+	
+}
