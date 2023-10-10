be075bcba4022d0b03ac7f33cab96bb3a7ff6950
S <s>
2023-10-10 16:25:28 +0400

    {"types":"C","runs":19}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -1,4 +1,22 @@
+import stanford.karel.Karel;
 
-public class CheckerboardKarel {
+public class CheckerboardKarel extends Karel{
+	
+	public void run(){
+		
+		 checkers();
+		
+	}
+	
+	private void checkers(){
+		
+		while(frontIsClear()){
+			
+			putBeeper();
+			move();
+			move();
+		}
+		
+	}
 
 }
