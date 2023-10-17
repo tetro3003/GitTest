05450c0f1fbbf5b77b975b76bf71702b842c1ccf
S <s>
2023-10-17 19:10:59 +0400

    {"types":"AR","runs":22}

---
+++ Pr19.java
@@ -0,0 +1,19 @@
+
+
+import acm.program.ConsoleProgram;
+
+public class Pr19 extends ConsoleProgram{
+	
+	public void run(){
+		
+		int a = readInt("Enter Number: ");
+		
+		for(int i = 0; i<a; i++){
+			
+			println("hello "+i);
+			
+		}
+		
+	}
+
+}
