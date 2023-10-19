839e078875190e212e07f40c19539dab34d0bea9
S <s>
2023-10-19 18:55:56 +0400

    {"types":"C","runs":17}

--- Pr22.java
+++ Pr22.java
@@ -1,10 +1,21 @@
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Pr22 extends GraphicsProgram {
 	
 	public void run(){
 		
-		
+		for(int a = 0; a<10; a++){
+			
+			for(int b = 0; b<10; b++){
+				
+				GRect rect = new GRect(20,20);
+				add(rect, b*20, a*20);
+				
+			}
+			
+			
+		}
 		
 	}
 
