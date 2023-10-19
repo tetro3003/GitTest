5ddd6f3995c1bf89a4b87de69e1761aeed23a13f
S <s>
2023-10-19 19:08:21 +0400

    {"types":"C","runs":25}

--- Pr22.java
+++ Pr22.java
@@ -7,34 +7,38 @@ public class Pr22 extends GraphicsProgram {
 	
 	public void run(){
 		
-		for(int a = 0; a<10; a++){
-			
-			for(int b = 0; b<10; b++){
-				
-				if(a%2==0&&b%2==0){
-				GRect rect = new GRect(20,20);
-				rect.setColor(Color.black);;
-				add(rect, b*20, a*20);
-				
-				} else if(a%2==1&&b%2==0){
-					GRect rect = new GRect(20,20);
-					add(rect, b*20, a*20);
-					
-				} else if(a%2==0&&b%2==1){
-					GRect rect = new GRect(20,20);
-					add(rect, b*20, a*20);
-						
-				} else if(a%2==1&&b%2==1){
-					GRect rect = new GRect(20,20);
-					rect.setColor(Color.black);;
-					add(rect, b*20, a*20);
-							
-							}
-				
-			
-			}
-			
-		}
+		GRect rect = new GRect(20,20);
+		rect.setColor(Color.black);;
+	add(rect, b*20, a*20);
+		
+//		for(int a = 0; a<10; a++){
+//			
+//			for(int b = 0; b<10; b++){
+//				
+//				if(a%2==0&&b%2==0){
+//				GRect rect = new GRect(20,20);
+//				rect.setColor(Color.black);;
+//				add(rect, b*20, a*20);
+//				
+//				} else if(a%2==1&&b%2==0){
+//					GRect rect = new GRect(20,20);
+//					add(rect, b*20, a*20);
+//					
+//				} else if(a%2==0&&b%2==1){
+//					GRect rect = new GRect(20,20);
+//					add(rect, b*20, a*20);
+//						
+//				} else if(a%2==1&&b%2==1){
+//					GRect rect = new GRect(20,20);
+//					rect.setColor(Color.black);
+//					add(rect, b*20, a*20);
+//							
+//					}
+//				
+//			
+//			}
+//			
+	//	}
 
 	}
 }
