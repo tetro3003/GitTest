276755595c0fd86f99a4b5c6ad622e5e9967e478
S <s>
2023-10-19 19:15:36 +0400

    {"types":"C","runs":31}

--- Pr22.java
+++ Pr22.java
@@ -11,7 +11,7 @@ public class Pr22 extends GraphicsProgram {
 			
 			for(int b = 0; b<8; b++){
 				
-					   if(a%2==0&&b%2==0){
+				if(a%2==0&&b%2==0){
 					GRect rect = new GRect(40,40);
 					rect.setFillColor(Color.black);;
 					add(rect, b*40, a*40);
