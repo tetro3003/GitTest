d262a55c6f2383debd58722a1cd4266612fe710d
S <s>
2023-10-20 10:52:13 +0400

    {"types":"C","runs":38}

--- Pr24.java
+++ Pr24.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class Pr24 {
+public class Pr24 extends ConsoleProgram {
+	
+	public void run(){
+		
+		int num = readInt("Enter Number: ");
+		checker(num);
+		
+	}
+	
+	private void checker(int a){
+		
+		int c=0;
+		
+		for(int b=0; b<a; a++){
+			
+			if(a%(b+1)==0) {
+				c++;
+			}
+			
+		}
+		
+		println(c);
+		
+	}
 
 }
