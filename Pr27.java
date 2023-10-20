11a37e015b6fcc18eadf7ad68a854331b49c9db9
S <s>
2023-10-20 11:12:34 +0400

    {"types":"C","runs":57}

--- Pr27.java
+++ Pr27.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Pr27 {
+public class Pr27 extends ConsoleProgram{
 
+	public void run(){
+		
+		int num1 = readInt("Enter Number N1: ");
+		int num2 = readInt("Enter Number N2: ");
+		
+		int b = num1;
+		
+		for(int a = 0; a<num2; a++){
+			
+			num1 *= b;
+			
+		}
+		
+		println(num1);
+		
+	}
+	
+	
+	
 }
