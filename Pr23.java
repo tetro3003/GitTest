e901dd74eb7cce4703a54ea7703f2d1d382c3cf9
S <s>
2023-10-20 10:48:31 +0400

    {"types":"C","runs":35}

--- Pr23.java
+++ Pr23.java
@@ -6,9 +6,14 @@ public class Pr23 extends ConsoleProgram{
 	
 	public void run(){
 		
-		int num = new readInt("Enter Number: ");
+		int num = readInt("Enter Number: ");
+		int b = 0;  
 		
+		for(int c = num; c>0; c--){
+			b += c;
+		}
 		
+		println(b);
 	}
 
 }
