4d84758aab5207ca923a2523648c24c156b14f05
S <s>
2023-10-20 11:17:15 +0400

    {"types":"C","runs":61}

--- Pr28.java
+++ Pr28.java
@@ -11,13 +11,16 @@ public class Pr28 extends ConsoleProgram {
 			
 			int b = readInt("Enter Number: ");
 			
+			if(b==-1){
+				println(c);
+				break;
+			}
+			
 			if(b%2==0){
 				c += 1;
 			}
 			
-			if(b==-1){
-				break;
-			}
+			
 			
 		}
 		
