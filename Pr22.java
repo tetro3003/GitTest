26415761283c7de3b22aa7978788bd16c5412697
S <s>
2023-10-20 10:47:03 +0400

    {"types":"ACR","runs":34}

--- Pr22.java
+++ Pr22.java
@@ -20,12 +20,8 @@ public class Pr22 extends GraphicsProgram {
 				} else if((a%2==1&&b%2==0)||(a%2==0&&b%2==1)){
 					GRect rect = new GRect(40,40);
 					add(rect, b*40, a*40);
-					
 				} 						
-			
-			}
-			
+			}	
 		}
-
 	}
 }
---
+++ Pr23.java
@@ -0,0 +1,14 @@
+
+
+import acm.program.ConsoleProgram;
+
+public class Pr23 extends ConsoleProgram{
+	
+	public void run(){
+		
+		int num = new readInt("Enter Number: ");
+		
+		
+	}
+
+}
