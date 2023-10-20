7fdec6af1c426709db200530810403fd5007c98e
S <s>
2023-10-20 11:38:22 +0400

    {"types":"C","runs":62}

--- Pr29.java
+++ Pr29.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Pr29 {
-
+public class Pr29 extends ConsoleProgram{
+	
+	public void run(){
+		
+	int a = readInt("Enter Number: ");
+	
+	int b = 1;
+	int c = 1;
+	int fibo = 0;
+	
+	for(int d=0; d < a; d++){
+		
+		c = fibo;
+		
+		fibo = b + fibo;
+		
+		b = c;
+	}
+	
+	println(fibo);
+}
 }
