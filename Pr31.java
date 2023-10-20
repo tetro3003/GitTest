c71ff8669e87501e135414b88664c5f9063b7b83
S <s>
2023-10-20 11:54:47 +0400

    {"types":"C","runs":67}

--- Pr31.java
+++ Pr31.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class Pr31 {
+public class Pr31 extends ConsoleProgram {
+	
+	public void run(){
+		int num = readInt("Enter Number: ");
+		simpleNumberChecker(num);
+		
+	}
+	private void simpleNumberChecker(int number){
+		
+		int a = 0;
+		
+		for(int i = 0; i < number; i++ ){
+			
+			if(number%(i+1)==0){
+				a++;
+			}
+			
+		}
+		
+		if(a>2) println("Your Number Is NOT Simple!");
+		else println("Your Number Is Simple!");
+		
+	}
+	
 
 }
