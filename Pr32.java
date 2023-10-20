df2edaa2502336d0eb02481cdbd27db19818d4dc
S <s>
2023-10-20 11:57:48 +0400

    {"types":"C","runs":69}

--- Pr32.java
+++ Pr32.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class Pr32 {
+public class Pr32 extends ConsoleProgram{
+	
+	public void run(){
+		
+		for(int i=1; i<=1000000; i++){
+			simpleNumberChecker(i);
+		}
+		
+	}
+
+	private void simpleNumberChecker(int number){
+		
+		int a = 0;
+		
+		for(int i = 0; i < number; i++ ){
+			
+			if(number%(i+1)==0){
+				a++;
+			}
+			
+		}
+		
+		if(a==2) println(number);
+		
+	}
 
 }
