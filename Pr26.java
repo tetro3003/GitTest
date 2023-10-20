5b2379a2b8a9ce30bb14c11ef3a376797d50f991
S <s>
2023-10-20 11:10:03 +0400

    {"types":"C","runs":54}

--- Pr26.java
+++ Pr26.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Pr26 {
-
+public class Pr26 extends ConsoleProgram{
+	
+	public void run(){
+		
+		int num1 = readInt("Enter Number N1: ");
+		int num2 = readInt("Enter Number N2: ");
+		
+		usg(num1, num2);
+		
+	}
+	
+	private void usg(int a, int b){
+		
+		for(int c=a; c>0; c--){
+			
+			if((a%c==0)&&(b%c==0))
+				{
+				println(c);
+				break;
+				}
+		}
+		
+	}
+	
 }
