5e3b3e7e6edf8c0da75bb4ba0951b6c17dee0a64
S <s>
2023-10-24 16:37:46 +0400

    {"types":"C","runs":81}

--- Pr32.java
+++ Pr32.java
@@ -6,24 +6,18 @@ public class Pr32 extends ConsoleProgram{
 		
 		for(int i=1; i<=1000000; i++){
 			simpleNumberChecker(i);
-		}
-		
+		}		
 	}
 
 	private void simpleNumberChecker(int number){
 		
 		int a = 0;
-		
+
 		for(int i = 0; i < number; i++ ){
-			
 			if(number%(i+1)==0){
 				a++;
-			}
-			
+			}	
 		}
-		
-		if(a==2) println(number);
-		
+		if(a==2) println(number);	
 	}
-
 }
